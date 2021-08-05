064439b1e2bbec4546088883bbd56cc4a885a3b0
Huzaifa <shaikhhuzaifa072@gmail.com>
2021-08-05 16:17:19 +0500

    skills

--- Components/Skills/Experience.js
+++ Components/Skills/Experience.js
@@ -1,7 +1,23 @@
 import { StylesProvider } from "@material-ui/styles";
 import style from "../../styles/Skills.module.scss";
-function Experience() {
-  return <div className={style.exp}></div>;
+function Experience({ details }) {
+  return (
+    <div className={style.exp}>
+      <h1 className={style.exp__title}>{details.expTitle}</h1>
+      <div className={style.exp__detail}>
+        <h2 className={style.exp__compTitle}>{details.companyName}</h2>
+        <p className={style.exp__period}>{details.expPeriod}</p>
+        <p className={style.exp__location}>
+          <span>Location: </span>
+          {details.location}
+        </p>
+        <p>
+          Company URL :<a href={details.compnayURL}>{details.companyName}</a>
+        </p>
+        <p>Projects: {details.project}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Experience;
--- pages/skills.js
+++ pages/skills.js
@@ -3,15 +3,41 @@ import SkillsContent from "../Components/Skills/SkillsContent";
 import SkillsStatus from "../Components/Skills/SkillsStatus";
 import style from "../styles/Skills.module.scss";
 function skills() {
+  const expDetails = [
+    {
+      expTitle: "Full Stack Developer",
+      expPeriod: "November 2020 –Present",
+      companyName: "DMS Global",
+      compnayURL: "https://dmsglobal.net/",
+      project: "DMS Universe",
+      location: "Bahrain",
+    },
+    {
+      expTitle: "Full Stack Developer",
+      expPeriod: "January 2019 – April 2020",
+      companyName: "Cohen International Group",
+      compnayURL: "https://dmsglobal.net/",
+      project: "Holidays By Saudia, Umrah By Saudia, Halatrip Travesys",
+      location: "Bahrain",
+    },
+    {
+      expTitle: "Full Stack Developer",
+      expPeriod: "October 2017 – January 2019",
+      companyName: "Doozielabs",
+      compnayURL: "http://doozielabs.com/",
+      project: "MilkADeal , iSay ,Eocean, Ramadan Legacy",
+      location: "Karachi, Pakistan",
+    },
+  ];
   return (
     <div className={style.skills}>
       <SkillsContent />
       <div className={style.skills__container}>
         <SkillsStatus />
         <div className={style.exp__container}>
-          <Experience />
-          <Experience />
-          <Experience />
+          {expDetails.map((detail) => {
+            return <Experience details={detail} key={detail.companyName} />;
+          })}
         </div>
       </div>
     </div>
